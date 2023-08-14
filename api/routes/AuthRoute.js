3336e18cdc5f0aadb55e013be489413d1c06cae0
Adrian Nads <marcelonads@gmail.com>
2023-08-15 01:13:30 +0800

    ADDED api route for login and register

---
+++ api/routes/AuthRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/api/auth/login', (req, res) => {
+
+});
+
+router.post('/api/auth/register', (req, res) => {
+
+})
+
+module.exports = router;
